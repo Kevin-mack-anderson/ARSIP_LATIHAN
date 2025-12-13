61d868c376270f389dd1ddccff6614c675c65027
Iruuz <pelajar24fairuzyahdimayassa@gmail.com>
2025-12-13 16:27:59 +0700

    beberapa perapihan kode dan penghapusan komentar

--- latihan_function.py
+++ latihan_function.py
@@ -2,124 +2,54 @@
 import os 
 
 os.system("cls")
-# # Heading section
-# def heading():
-#     print(f"{'HALO SELAMAT DATANG USER' :^40}")
-#     print(f"{'SENANG BERTEMU LAGI' :^40}")
-#     print(f"{'=' * 40 :^40}")
+# Menghitung Keliling Persegi Panjang
+# Heading section
+def heading():
+    print(f"{'HALO SELAMAT DATANG USER' :^40}")
+    print(f"{'SENANG BERTEMU LAGI' :^40}")
+    print(f"{'=' * 40 :^40}")
 
-# # Inputan User
-# def input_user():
-#     panjang = int(input("Masukan Panjang : "))
-#     lebar = int(input("Masukan Lebar : "))
-#     return panjang,lebar
-# # Penghitungan Luas
-# def hitung_luas(lebar,panjang):
-#     return lebar * panjang 
-# # Penghitungan Keliling
-# def hitung_keliling(lebar,panjang):
-#     return 2 * (PANJANG + LEBAR)
-# # Display
-# def display(message,value):
-#     print(f"Hasil dari {message} = {value}")
-# # Output Keluaran
-# while True:
-#   heading()
-# #   Pilihan operasi
-#   pilihan = input("Mau Melakukan Apa? (luas/keliling) : ")
-#   LEBAR,PANJANG = input_user()
-# #   Flow Pilihan
-#   if pilihan == "luas":
-#      LUAS = hitung_luas(LEBAR,PANJANG)
-#      display("Luas",LUAS )
-#   elif pilihan == "keliling":
-#       KELILING = hitung_keliling(LEBAR,PANJANG) 
-#       display("Keliling",KELILING )
-#   else: print("tidak ada")
-# #   LUAS = hitung_luas(LEBAR,PANJANG)
-# #   KELILING = hitung_keliling(LEBAR,PANJANG)
-# #   display("Luas",LUAS )
-# #   display("Keliling",KELILING )
-# #   Main Program
-#   in_continue = input("Apakah Mau Lagi?(y/n) :  ").lower()
-#   if in_continue == "n":
-#      print("terimakasih banyak") 
-#      break
-
-
-# Buat Struk Pembelian
-
-# # Bagian Heading
-# def head_struk():
-#     print(f"{'TAMBAL BAN SEJATI' :^40}")
-#     print(f"{'MENYEDIAKAN BERBAGAI JENIS BAN' :^40}")
-#     print(f"{'=' * 44 :^40}")
-# # Bagian Keterangan Table
-# def isi_table():
-#     print(f"{'No' :<5} {'Tipe Ban' :<10} {'Merk' :>10} {'Harga' :>12}")
-#     print(f"{'1' :<5} {'Soft' :<10} {'Pirelli' :>10} {'Rp.700.000' :>15}")
-#     print(f"{'2' :<5} {'Medium' :<10} {'Continental' :>10}")
-#     print(f"{'3' :<5} {'Hard' :<10} {'Swallow' :>10}")
-# # Data Setiap Ban
-# def data_ban():
-#     pireli = {'harga' : 700000 }
-#     continental = {'harga2' : 600000}
-#     swallow = {'harga3' : 400000}
-#     return pireli,continental,swallow
-# # Inputan user
-# def inputan():
-#     pilih = input("Ketikan Ban Yang Dibeli Menggunakan Format Angka : ")
-#     qty = int(input("Masukan Jumlah Beli : "))
-#     return pilih,qty
-# # Penghitungan
-# def hitung_total(pilihan,quanty):
+# Inputan User
+def input_user():
+    panjang = int(input("Masukan Panjang : "))
+    lebar = int(input("Masukan Lebar : "))
+    return panjang,lebar
+# Penghitungan Luas
+def hitung_luas(lebar,panjang):
+    return lebar * panjang 
+# Penghitungan Keliling
+def hitung_keliling(lebar,panjang):
+    return 2 * (PANJANG + LEBAR)
+# Display
+def display(message,value):
+    print(f"Hasil dari {message} = {value}")
+# Output Keluaran
+while True:
+  heading()
+#   Pilihan operasi
+  pilihan = input("Mau Melakukan Apa? (luas/keliling) : ")
+  LEBAR,PANJANG = input_user()
+#   Flow Pilihan
+  if pilihan == "luas":
+     LUAS = hitung_luas(LEBAR,PANJANG)
+     display("Luas",LUAS )
+  elif pilihan == "keliling":
+      KELILING = hitung_keliling(LEBAR,PANJANG) 
+      display("Keliling",KELILING )
+  else: print("tidak ada")
+  LUAS = hitung_luas(LEBAR,PANJANG)
+  KELILING = hitung_keliling(LEBAR,PANJANG)
+  display("Luas",LUAS )
+  display("Keliling",KELILING )
+  Main Program
+  in_continue = input("Apakah Mau Lagi?(y/n) :  ").lower()
+  if in_continue == "n":
+     print("terimakasih banyak") 
+     break
 
 
-# # Outnya
-# head_struk()
-# isi_table()
-# BAN1,BAN2,BAN3 = data_ban()
-# PILIHAN,QTY = inputan()
 
-# # Bagian Heading
-# print(f"{'TAMBAL BAN SEJATI' :^40}")
-# print(f"{'MENYEDIAKAN BERBAGAI JENIS BAN' :^40}")
-# print(f"{'=' * 44 :^40}")
-# # Bagian data
-# D_PIRELLI = ["Pirelli",'Soft',700000 ]
-# D_CONTINENTAL = ["Continental",'Very Soft',900000 ]
-# D_SWALLOW = ["Swallow",'Medium',500000 ]
-# # Data kedalam dict
-# DATA_BAN = {
-#     'MRK_001':D_PIRELLI,
-#     'HRG_001':D_PIRELLI[2],
-#     'MRK_002':D_CONTINENTAL,
-#     'HRG_002':D_CONTINENTAL[2],
-#     'MRK_003':D_SWALLOW,
-#     'HRG_003':D_SWALLOW[2]
-# }
-# # Bagian Table
-# print(f"{'No' :<5} {'Tipe Ban' :<10} {'Merk' :>10} {'Harga' :>12}")
-# print(f"{'1' :<5} {'Pirelli' :<10} {'Pirelli' :>10} {'Rp.700.000' :>15}")
-# print(f"{'2' :<5} {'Medium' :<10} {'Continental' :>10}")
-# print(f"{'3' :<5} {'Hard' :<10} {'Swallow' :>10}")
-# # Inputan user
-# pilihan = input("Pilihan Berdasarkan Nomor : ")
-# qty = int(input("Quanty : "))
-# # Penghitungan
-# if pilihan  == "1":
-#     subtotal = DATA_BAN['HRG_001'] * qty
-# elif pilihan == "2":
-#     subtotal = DATA_BAN['HRG_002'] * qty
-# elif pilihan == "3":
-#     subtotal = DATA_BAN['HRG_003'] * qty
-# print(subtotal)
-# # Masukan Jumlah Uang Yang Dibayar
-# uang_masuk = int(input("Masukan Nominal Uang : "))
-# # Proses
-# hasil = uang_masuk - subtotal
-# print(hasil)
-
+# JUALAN BAN MOTOR
 # Implementasi kedalam function
 # Heading Section
 def heading_sn():
